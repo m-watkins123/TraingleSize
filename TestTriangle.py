dba4591f29f05de965c2a896d16865f17570df63
Manassah Watkins <manassahwatkins@gmail.com>
2021-09-18 00:44:38 -0400

    Update TestTriangle.py

--- TestTriangle.py
+++ TestTriangle.py
@@ -1,32 +1,34 @@
 # -*- coding: utf-8 -*-
-"""
-Updated Jan 21, 2018
-The primary goal of this file is to demonstrate a simple unittest implementation
-
-@author: jrr
-@author: rk
-"""
-
 import unittest
 
 from Triangle import classifyTriangle
-
-# This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
-
 class TestTriangles(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
+def testRightTriangleA(self): 
+        self.assertEqual(classifyTriangle(5,6,1),'Scalene','5,6,1 is a obtuse triangle')
 
-    def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
-
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+def testRightTriangleB(self): 
+        self.assertEqual(classifyTriangle(5,7,4),'Scalene','5,7,4 is a obtuse triangle')
         
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+def testEquilateralTrianglesC(self): 
+        self.assertEqual(classifyTriangle(1,1,1),'isocles','1,1,1 should be isocles')
+A = input ("Enter side A")
+
+B = input ("Enter side B")
+
+C = input ("Enter side C")
+
+if A == B == C:
+	print("Equal Sides")
+	pass
+elif A == B or B == C or C == A:
+	print("Isocles")
+	pass
+else:
+	print ("Scalene")
+	pass
 
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
+
 
